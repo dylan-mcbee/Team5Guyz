ec4ff5741c4e8dfe0017beb58a4598606e00d420
latezbradley <bradley.latez@gmail.com>
2017-07-03 20:24:48 -0400

    Update ajaxFactory.js

--- Project2/Cinema/src/main/webapp/static/js/ajaxFactory.js
+++ Project2/Cinema/src/main/webapp/static/js/ajaxFactory.js
@@ -3,21 +3,8 @@
  */
 app.factory('ajaxFactory', function($http){
 	let factory = {}
-<<<<<<< HEAD
-
-	var domain = 'http://localhost:8083/Cinema'	
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-	var domain = 'http://localhost:8083/Cinema'
-=======
 	var domain = 'http://localhost:8081/Cinema'
->>>>>>> d0c2dc606488e91d6eadf028f4b46ad6ab3a3d8f
-=======
-	var domain = 'http://localhost:8081/Cinema'
->>>>>>> 9d824d420b0fac122a49296497844e30c48ebfee
 		
->>>>>>> master
 	factory.getUserInfo = function(credential, controller){
 		return $http.post(domain +controller, credential);
 	};
